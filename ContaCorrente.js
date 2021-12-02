e924e8a7a9e55c61fff715735b5cbf14ec02225e
Isabel O <isaolivcld@gmail.com>
2021-12-02 15:52:43 -0300

    separando as classes pra ficar mais organizado

---
+++ ContaCorrente.js
@@ -0,0 +1,30 @@
+
+
+class ContaCorrente {
+    agencia;
+    saldo;
+
+    sacar(valor){
+         if (this.saldo >= valor){
+             this.saldo -= valor; 
+             
+         } 
+        
+
+         else {
+             console.log("Você não tem saldo o suficiente para realizar o saque")
+         }
+
+         
+    }
+
+    depositar(valor) {
+        if (valor > 0) {
+            this.saldo += valor; /* aqui eu posso colocar: if (valor<0) return; daí esse return vai querer dizer que, se o valor for menor que zero, não é pra executar
+            nada no código, porque vai "retornar a função". Daí não vai executar nada, e o nome é early return. */
+                                
+        } else {
+            console.log ("valor de depósito inválido, tente novamente");
+        }
+    }
+}
