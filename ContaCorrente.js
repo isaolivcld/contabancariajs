cf05b5d3a1379b7d5963525c12bd2214791667a5
Isabel O <isaolivcld@gmail.com>
2021-12-02 18:11:10 -0300

    integração de classes e utilização do JSON pra integrar arquivos diferentes no JS

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,11 +2,13 @@
 
 export class ContaCorrente {
     agencia;
-    saldo;
+    cliente;
+        
+    _saldo; // quando eu quiser mostrar que não é pra alterar um código, basta colocar um _ na frente dele. Por exemplo: _saldo 
 
     sacar(valor){
-         if (this.saldo >= valor){
-             this.saldo -= valor; 
+         if (this._saldo >= valor){
+             this._saldo -= valor; 
              
          } 
         
@@ -20,11 +22,11 @@ export class ContaCorrente {
 
     depositar(valor) {
         if (valor > 0) {
-            this.saldo += valor; /* aqui eu posso colocar: if (valor<0) return; daí esse return vai querer dizer que, se o valor for menor que zero, não é pra executar
+            this._saldo += valor; /* aqui eu posso colocar: if (valor<0) return; daí esse return vai querer dizer que, se o valor for menor que zero, não é pra executar
             nada no código, porque vai "retornar a função". Daí não vai executar nada, e o nome é early return. */
                                 
         } else {
             console.log ("valor de depósito inválido, tente novamente");
         }
     }
-} // quando eu quiser mostrar que não é pra alterar um código, basta colocar um _ na frente dele. Por exemplo: _saldo 
+} 
--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 
 const cliente1 = new Cliente();
+cliente1.nome = `Isabel Oliveira`;
+cliente1.cpf = 111232123221;
 
-const contaCorrenteRafaelaRiggs = new ContaCorrente();
+const cliente2 = new Cliente ();
+cliente2.nome = `Cyper Notorius`;
+cliente2.cpf = 56545645465;
 
-contaCorrenteRafaelaRiggs.nome="Rafaela Riggs";
-contaCorrenteRafaelaRiggs.agencia=18472;
-contaCorrenteRafaelaRiggs.saldo=500;
-contaCorrenteRafaelaRiggs.depositar(500);
-contaCorrenteRafaelaRiggs.sacar(50);
+const contaCorrente1 = new ContaCorrente();
+
+contaCorrente1.cliente = cliente1
+contaCorrente1.agencia=18472;
+contaCorrente1._saldo=500;
+contaCorrente1.depositar(500);
+contaCorrente1.sacar(50);
 
 
-console.log(contaCorrenteRafaelaRiggs);
+console.log(contaCorrente1);
+
+
+const contaCorrente2 = new ContaCorrente ();
+
+contaCorrente2.cliente = cliente2;
+contaCorrente2.agencia= 4568;
+contaCorrente2._saldo=100
+contaCorrente2.depositar(900);
+contaCorrente2.sacar(200); //eu errei fazendo contaCorrente2.sacar=(200); numa vez aí, e aí a propriedade aparecia 100% fiel, ao invés de apenas realizar a operação de saque. Então, 
+// é legal ter em mente que o sinal de igual é só pra quando eu vou escrever o valor da propriedade de forma direta ali, e não pra realizar uma situação de dependência 
+//entre parâmetros pré-estabelecidos, por exemplo, o if{..}/else{..}
+
+console.log (contaCorrente2);
 
 
