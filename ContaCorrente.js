517806825f56bea9d1f19dcc4b532e40da88facd
Isabel O <isaolivcld@gmail.com>
2021-12-03 17:45:31 -0300

    bug de transferência bancária corrigido

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,32 +1,33 @@
-
-
 export class ContaCorrente {
     agencia;
     cliente;
-        
+
     _saldo; // quando eu quiser mostrar que não é pra alterar um código, basta colocar um _ na frente dele. Por exemplo: _saldo 
 
-    sacar(valor){
-         if (this._saldo >= valor){
-             this._saldo -= valor; 
-             
-         } 
-        
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
 
-         else {
-             console.log("Você não tem saldo o suficiente para realizar o saque")
-         }
+        } else {
+            console.log("Você não tem saldo o suficiente para realizar o saque")
+        }
 
-         
+
     }
 
     depositar(valor) {
         if (valor > 0) {
-            this._saldo += valor; /* aqui eu posso colocar: if (valor<0) return; daí esse return vai querer dizer que, se o valor for menor que zero, não é pra executar
-            nada no código, porque vai "retornar a função". Daí não vai executar nada, e o nome é early return. */
-                                
-        } else {
-            console.log ("valor de depósito inválido, tente novamente");
+            this._saldo += valor;
+        }
+        /* aqui eu posso colocar: if (valor<0) return; daí esse return vai querer dizer que, se o valor for menor que zero, não é pra executar
+                   nada no código, porque vai "retornar a função". Daí não vai executar nada, e o nome é early return. */
+        else {
+            console.log("valor de depósito inválido, tente novamente");
         }
     }
-} 
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor)
+        conta.depositar(valorSacado);
+    }
+}
